08dc0e2c679e9cbffb2d91f276c9c2dc67f1b9fb
Marshall Brown <marshallendel@gmail.com>
2021-11-09 20:57:48 -0500

    mute button

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ class Drumkit {
     this.pads = document.querySelectorAll(".pad");
     this.playButton = document.querySelector(".play");
     this.select = document.querySelectorAll("select");
+    this.mutes = document.querySelectorAll(".mute");
     this.kickAudio = document.querySelector(".kick-sound");
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
@@ -24,6 +25,12 @@ class Drumkit {
     this.select.forEach((select) => {
       select.addEventListener("change", (e) => {
         this.changeSound(e);
+      });
+    });
+
+    this.mutes.forEach((mute) => {
+      mute.addEventListener("click", (e) => {
+        this.muteSound(e);
       });
     });
   }
@@ -82,6 +89,38 @@ class Drumkit {
       case "hihat-select":
         this.hihatAudio.src = e.target.value;
         break;
+    }
+  }
+
+  muteSound(e) {
+    let dataTrack = e.target.dataset.track;
+
+    if (e.target.classList.contains("active")) {
+      switch (dataTrack) {
+        case "0":
+          this.kickAudio.volume = 1;
+          break;
+        case "1":
+          this.snareAudio.volume = 1;
+          break;
+        case "2":
+          this.hihatAudio.volume = 1;
+          break;
+      }
+      e.target.classList.toggle("active");
+    } else {
+      switch (dataTrack) {
+        case "0":
+          this.kickAudio.volume = 0;
+          break;
+        case "1":
+          this.snareAudio.volume = 0;
+          break;
+        case "2":
+          this.hihatAudio.volume = 0;
+          break;
+      }
+      e.target.classList.toggle("active");
     }
   }
 }
